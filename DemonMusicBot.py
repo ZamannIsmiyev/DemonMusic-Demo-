050b7cd6f7342e398911919336f9c9a60dde3283
ZamannIsmiyev <99416682+ZamannIsmiyev@users.noreply.github.com>
2022-05-22 16:44:41 +0400

    Update DemonMusicBot.py

--- DemonMusicBot.py
+++ DemonMusicBot.py
@@ -52,10 +52,10 @@ def help(client, message):
         quote=False,
         reply_markup=InlineKeyboardMarkup(
             [[
-                    InlineKeyboardButton('Rəsmi Kanal ✅', url='https://t.me/Botsinator'),
-                    InlineKeyboardButton('Playlist 🎵', url=f'https://t.me/{Config.PLAYLIST_NAME}')
+                    InlineKeyboardButton('Söhbət Qrupu ✅', url='https://t.me/AsoSonZirve'),
+                    InlineKeyboardButton('Playlist 🎵', url=f'https://t.me/{Config.ASOSonZirve}')
                   ],[
-                    InlineKeyboardButton('Sahib 👨🏻‍💻', url=f'T.me/{Config.BOT_OWNER}')
+                    InlineKeyboardButton('Sahib 👨🏻‍💻', url=f'T.me/{Config.ismiyev95}')
                 ]
             ]
         )
@@ -111,7 +111,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎵 Yüklədi [Music Bot](https://t.me/{Config.BOT_USERNAME})"
+        rep = f"🎵 Yüklədi [Music Bot](https://t.me/{Config.AsoSonZirve})"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
